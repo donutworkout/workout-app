73a77a52bbaab3dc9891fd5c8b16c401144364c5
dawnhazed <shafadoesthings@gmail.com>
2025-10-14 16:59:45 +0700

    project setup

---
+++ WorkoutWacthApp Watch App/WorkoutWacthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutWacthAppApp.swift
+//  WorkoutWacthApp Watch App
+//
+//  Created by Nadaa Shafa Nadhifa on 14/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutWacthApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
