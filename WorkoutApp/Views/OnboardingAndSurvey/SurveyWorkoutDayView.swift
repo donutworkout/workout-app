04c0d0cc2c7e86edee4e27b85fbf54239211b521
jeniiiiiii <jennifertjioe@gmail.com>
2025-10-23 23:38:20 +0700

    fix: fix the survey section

---
+++ WorkoutApp/Views/OnboardingAndSurvey/SurveyWorkoutDayView.swift
@@ -0,0 +1,18 @@
+//
+//  SurveyWorkoutDayView.swift
+//  WorkoutApp
+//
+//  Created by Jennifer Evelyn on 23/10/25.
+//
+
+import SwiftUI
+
+struct SurveyWorkoutDayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SurveyWorkoutDayView()
+}
---
+++ WorkoutApp/Views/OnboardingAndSurvey/SurveyWorkoutLevelView.swift
@@ -0,0 +1,18 @@
+//
+//  SurveyWorkoutLevelView.swift
+//  WorkoutApp
+//
+//  Created by Jennifer Evelyn on 23/10/25.
+//
+
+import SwiftUI
+
+struct SurveyWorkoutLevelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SurveyWorkoutLevelView()
+}
