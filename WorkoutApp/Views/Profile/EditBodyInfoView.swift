c67750fb4ecfa67411eacffe237e8c5ebdc55d07
jeniiiiiii <jennifertjioe@gmail.com>
2025-10-31 14:04:20 +0700

    fix: fix the router

---
+++ WorkoutApp/Views/Profile/EditBodyInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  EditBodyInfo.swift
+//  WorkoutApp
+//
+//  Created by Jennifer Evelyn on 30/10/25.
+//
+
+import SwiftUI
+
+struct EditBodyInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditBodyInfo()
+}
