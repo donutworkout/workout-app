3bee3563c7d6443e85985cb4193df9cc5f2eaf14
nadaa shafa <137472619+dawnhazed@users.noreply.github.com>
2025-11-27 13:32:16 +0700

    fix : follicular circles on profile

--- WorkoutApp/Views/Profile/ProfileView.swift
+++ WorkoutApp/Views/Profile/ProfileView.swift
@@ -349,9 +349,9 @@ struct ProfileView: View {
     private func calculateOvulationDates() {
         ovulationDates.removeAll()
         guard !menstrualDates.isEmpty else { return }
-        let lastMenstrualDate = menstrualDates.sorted().last!
-        if let ovulationStart = calendar.date(byAdding: .day, value: 7, to: lastMenstrualDate) {
-            for i in 0..<14 {
+        let firstMenstrualDate = menstrualDates.sorted().first!
+        if let ovulationStart = calendar.date(byAdding: .day, value: 5, to: firstMenstrualDate) {
+            for i in 0..<7 {
                 if let ovulationDay = calendar.date(byAdding: .day, value: i, to: ovulationStart) {
                     ovulationDates.insert(ovulationDay)
                 }
